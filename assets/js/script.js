accaeb0a893dd5e23aa0775018deb0a783a8a68b
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-07 23:50:45 +0100

    Create a function to return the select pallet type value and change the values of the pallet inputs to numbers in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,24 @@ const calculateResult = document.getElementById('calculatePallet');
 const palletNumberInput = document.getElementById('palletNumber');
 const loadingHeightInput = document.getElementById('loadingHeight');
 const loadingWeightInput = document.getElementById('loadingWeight');
-const euroPalletinput = document.getElementById('euroPallet');
-
-
+const palletTypeinput = document.getElementById('palletType');
 
 function calculateTotal() {
-    const palletNumberValue = palletNumberInput.value;
-    const loadingHeightValue = loadingHeightInput.value;
-    const loadingWeightValue = loadingWeightInput.value;
-    const euroPalletValue = euroPalletinput.value;
-    console.log(euroPalletValue);
+    const palletTypeValue = palletTypeinput.value;
+    const palletValue = palletTypeValue * 100
+    console.log(palletValue);
 }
+
+
+
+
+// function calculateTotal() {
+//     const palletNumberValue = palletNumberInput.value;
+//     const loadingHeightValue = loadingHeightInput.value;
+//     const loadingWeightValue = loadingWeightInput.value;
+//     const palletTypeValue = palletTypeinput.value;
+
+
+// }
 
 calculateResult.addEventListener('click', calculateTotal)
