a71eadac7b05c0bc72e2c30a08fa99a00a349e2c
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-07 22:25:29 +0100

    test all the pallet data form inputs by using click event listener function to console.log the values of the inputs and testing with devtools

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,16 @@ const calculateResult = document.getElementById('calculatePallet');
 const palletNumberInput = document.getElementById('palletNumber');
 const loadingHeightInput = document.getElementById('loadingHeight');
 const loadingWeightInput = document.getElementById('loadingWeight');
+const euroPalletinput = document.getElementById('euroPallet');
 
 
 
 function calculateTotal() {
-    console.log("clicked")
+    const palletNumberValue = palletNumberInput.value;
+    const loadingHeightValue = loadingHeightInput.value;
+    const loadingWeightValue = loadingWeightInput.value;
+    const euroPalletValue = euroPalletinput.value;
+    console.log(euroPalletValue);
 }
 
 calculateResult.addEventListener('click', calculateTotal)
