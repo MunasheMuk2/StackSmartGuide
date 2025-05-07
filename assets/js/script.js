b0685f273653757b94a066cbf57cc9d940425e4f
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-07 21:50:44 +0100

    Add pallet input data in a div instead and create inputs without bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+
+// const palletWeight = document.getElementById("palletWeight");
+// const palletHeight = document.getElementById("palletHeight");
+// const palletOne = document.getElementById("palletOne");
+// const palletHTwo = document.getElementById("palletTwo");
+// const recommendation = document.getElementById("recommendation");
+
+
+function userChoice() {
+
+    const palletAmount = document.getElementById('palletAmount');
+
+    if (palletAmount = 1) {
+        return 'Sprinter van required'
+    } else {
+        return 'Please refer to the guide for specific vehicle dimensions for your goods'
+    }
+};
+
+function vehicleType() {
+    vehicle.innerHTML = userChoice()
+    console.log(userChoice())
+}
