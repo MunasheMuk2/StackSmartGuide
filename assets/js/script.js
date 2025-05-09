c1d4f1f81547440d33d51c488becf92f66519cbe
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-09 09:01:18 +0100

    move the code from the onchange function to on click so you can use calculate button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,24 @@ const calculateResult = document.getElementById("calculatePallet");
 // const palletTypeInput = document.querySelector('#palletType').options[0].selected = true;
 
 function calculatePallets() {
-    const palletNumberInput = document.getElementById('palletType').value;
+    // const palletNumberInput = document.getElementById('palletNumber').value;
+    // const loadingWeightInput = document.getElementById('loadingWeight').value;
+
+    // palletsWeight = palletNumberInput * loadingWeightInput
+
+    // const loadingHeightInput = document.getElementById('loadingHeight').value;
+    // const euroPallet = document.querySelector('#euro').value = 120 * 80;
+    // const industrialPallet = document.querySelector('#industrial').value = 120 * 100;
+
+    // euroDimensions = parseInt(euroPallet) + parseInt(loadingHeightInput)
+    // industrialDimensions = parseInt(industrialPallet) + parseInt(loadingHeightInput)
+
+    // console.log(palletsWeight)
+
+}
+
+function calculateTotal() {
+    const palletNumberInput = document.getElementById('palletNumber').value;
     const loadingWeightInput = document.getElementById('loadingWeight').value;
 
     palletsWeight = palletNumberInput * loadingWeightInput
@@ -19,10 +36,7 @@ function calculatePallets() {
     euroDimensions = parseInt(euroPallet) + parseInt(loadingHeightInput)
     industrialDimensions = parseInt(industrialPallet) + parseInt(loadingHeightInput)
 
-}
-
-function calculateTotal() {
-
+    console.log(euroDimensions)
 };
 
 
