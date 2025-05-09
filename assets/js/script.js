f748b8b2914499701e0b500b5227e015b3ea4bdf
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-09 08:14:33 +0100

    Add onchange functions to html and create function in js and create an event listener for the calculate button and create a function fot that

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
-const calculateResult = document.getElementById('calculatePallet');
-const palletNumberInput = document.getElementById('palletNumber');
-const loadingHeightInput = document.getElementById('loadingHeight');
-const loadingWeightInput = document.getElementById('loadingWeight');
-const palletTypeinput = document.getElementById('palletType');
+const calculateResult = document.getElementById("calculatePallet");
 
-function calculateTotal() {
-    const palletTypeValue = palletTypeinput.value;
-    const palletValue = palletTypeValue * 100
-    console.log(palletValue);
+
+// const palletNumberInput = document.getElementById('palletNumber').value;
+// const loadingHeightInput = document.getElementById('loadingHeight').value;
+// const loadingWeightInput = document.getElementById('loadingWeight').value;
+// const palletTypeInput = document.querySelector('#palletType').options[0].selected = true;
+
+function calculatePallets() {
+    const palletNumberInput = document.getElementById('palletNumber').value;
+    const loadingWeightInput = document.getElementById('loadingWeight').value;
+
+    palletsWeight = palletNumberInput * loadingWeightInput
+
+    euroPallet = document.querySelector('#palletType').selectedOptions[0].value;
+    const loadingHeightInput = document.getElementById('loadingHeight').value;
+
+    palletsDimensions = parseInt(euroPallet) * parseInt(loadingHeightInput)
+
+    console.log(euroPallet)
+
 }
 
+function calculateTotal() {
+
+};
 
 
-
-// function calculateTotal() {
-//     const palletNumberValue = palletNumberInput.value;
-//     const loadingHeightValue = loadingHeightInput.value;
-//     const loadingWeightValue = loadingWeightInput.value;
-//     const palletTypeValue = palletTypeinput.value;
-
-
-// }
-
-calculateResult.addEventListener('click', calculateTotal)
+calculateResult.addEventListener("click", calculateTotal)
