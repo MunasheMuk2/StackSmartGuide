7caa626814ddebc935ee0427c1ad1f3507581169
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-09 08:30:50 +0100

    Add ids to the pallet type options and create a variable that gives each option the intended value

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,17 @@ const calculateResult = document.getElementById("calculatePallet");
 // const palletTypeInput = document.querySelector('#palletType').options[0].selected = true;
 
 function calculatePallets() {
-    const palletNumberInput = document.getElementById('palletNumber').value;
+    const palletNumberInput = document.getElementById('palletType').value;
     const loadingWeightInput = document.getElementById('loadingWeight').value;
 
     palletsWeight = palletNumberInput * loadingWeightInput
 
-    euroPallet = document.querySelector('#palletType').selectedOptions[0].value;
     const loadingHeightInput = document.getElementById('loadingHeight').value;
+    const euroPallet = document.querySelector('#euro').value = 120 * 80;
 
     palletsDimensions = parseInt(euroPallet) * parseInt(loadingHeightInput)
 
-    console.log(euroPallet)
+    console.log(palletsDimensions)
 
 }
 
