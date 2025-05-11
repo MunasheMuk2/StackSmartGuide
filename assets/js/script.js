c4d4ea45df1046fd7f01d693009a7ba64559cb7e
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-05-11 20:35:20 +0100

    Add else statement to advise user no suitable vehicle found if the vehicles in the object do not fit their goods

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,24 +82,13 @@ function calculateTotal() {
     };
 
 
-    //Eurodimensions figure worked out by adding loading height to the pallet dims (120x80 & 120 x 100) and multipied by the number of pallets the users enters on the form so total mass can be calculated
+    //If statements based on selection of Europallet and user inputs of weight etc - else if applied to ensure the vehicle recommandation is based on the smallest vehicle that can take the total goods
 
-
-
-
-    // Calculations for euro pallets - Dimensions limit set based on total for euroDimensions/industrialDimensions multiplied by the total pallets the vehicle can fit 
-
-    // Pallet dimensions set between values to ensure the vehicle recommandation is based on the smallest vehicle that can take the goods
-    // let smallVan;
 
     if (euroDimensions <= 1056000 && palletsWeight <= vehicleLimits.smallVan.maxWeight &&
         palletHeightInput <= vehicleLimits.smallVan.maxHeight &&
         palletNumberInput <= vehicleLimits.smallVan.maxPallets) {
         vehicle.innerHTML = "Small Van"
-    } else if (euroDimensions <= 1248000 && palletsWeight <= vehicleLimits.shortWheelBase.maxWeight &&
-        palletHeightInput <= vehicleLimits.shortWheelBase.maxHeight &&
-        palletNumberInput <= vehicleLimits.shortWheelBase.maxPallets) {
-        vehicle.innerHTML = "Short Wheel Base"
     } else if (euroDimensions <= 1248000 && palletsWeight <= vehicleLimits.shortWheelBase.maxWeight &&
         palletHeightInput <= vehicleLimits.shortWheelBase.maxHeight &&
         palletNumberInput <= vehicleLimits.shortWheelBase.maxPallets) {
@@ -139,77 +128,52 @@ function calculateTotal() {
     }
 
 
-    // // Calculations for industrial pallets 
-    // // let smallVan;
+    // Calculations for industrial pallets 
 
-    // if (euroDimensions <= 12110 && palletsWeight <= 400) {
+    if (industrialPallet <= 1320000 && palletsWeight <= vehicleLimits.smallVan.maxWeight &&
+        palletHeightInput <= vehicleLimits.smallVan.maxHeight &&
+        palletNumberInput <= vehicleLimits.smallVan.maxPallets) {
+        vehicle.innerHTML = "Small Van"
+    } else if (industrialPallet <= 1560000 && palletsWeight <= vehicleLimits.shortWheelBase.maxWeight &&
+        palletHeightInput <= vehicleLimits.shortWheelBase.maxHeight &&
+        palletNumberInput <= vehicleLimits.shortWheelBase.maxPallets) {
+        vehicle.innerHTML = "Short Wheel Base"
+    } else if (industrialPallet <= 2040000 && palletsWeight <= vehicleLimits.longWheelBase.maxWeight &&
+        palletHeightInput <= vehicleLimits.longWheelBase.maxHeight &&
+        palletNumberInput <= vehicleLimits.longWheelBase.maxPallets) {
+        vehicle.innerHTML = "Long Wheel Base"
+    } else if (industrialPallet <= 2100000 && palletsWeight <= vehicleLimits.smallVan.maxWeight &&
+        palletHeightInput <= vehicleLimits.smallVan.maxHeight &&
+        palletNumberInput <= vehicleLimits.smallVan.maxPallets) {
+        vehicle.innerHTML = "Sprinter van"
+    } else if (industrialPallet <= 2220000 && palletsWeight <= vehicleLimits.lutonVan.maxWeight &&
+        palletHeightInput <= vehicleLimits.lutonVan.maxHeight &&
+        palletNumberInput <= vehicleLimits.lutonVan.maxPallets) {
+        vehicle.innerHTML = "Luton van"
+    } else if (industrialPallet <= 2760000 && palletsWeight <= vehicleLimits.sevenHalfTonne.maxWeight &&
+        palletHeightInput <= vehicleLimits.sevenHalfTonne.maxHeight &&
+        palletNumberInput <= vehicleLimits.sevenHalfTonne.maxPallets) {
+        vehicle.innerHTML = "7 & a half tonne truck"
+    } else if (industrialPallet <= 3000000 && palletsWeight <= vehicleLimits.eighteenTonne.maxWeight &&
+        palletHeightInput <= vehicleLimits.eighteenTonne.maxHeight &&
+        palletNumberInput <= vehicleLimits.eighteenTonne.maxPallets) {
+        vehicle.innerHTML = "18 Tonne"
+    } else if (industrialPallet >= 3000001 && industrialPallet <= 2880000 && palletsWeight <= vehicleLimits.twentySixTonne.maxWeight &&
+        palletHeightInput <= vehicleLimits.twentySixTonne.maxHeight &&
+        palletNumberInput <= vehicleLimits.twentySixTonne.maxPallets) {
+        vehicle.innerHTML = "26 Tonne"
+    } else if (industrialPallet <= 3240000 && palletsWeight <= vehicleLimits.artic.maxWeight &&
+        palletHeightInput <= vehicleLimits.artic.maxHeight &&
+        palletNumberInput <= vehicleLimits.artic.maxPallets) {
+        vehicle.innerHTML = "Artic"
+    } else if (industrialPallet >= 2880000 && industrialPallet <= 3600000 && palletsWeight <= vehicleLimits.megatrailer.maxWeight &&
+        palletHeightInput <= vehicleLimits.megatrailer.maxHeight &&
+        palletNumberInput <= vehicleLimits.megatrailer.maxPallets) {
+        vehicle.innerHTML = "Megatrailer"
+    } else {
+        vehicle.innerHTML = "No suitable vehicle found"
+    }
 
-    //     vehicle.innerHTML = "Small Van"
-    // }
-
-    // //Short Wheel Base
-
-    // if (euroDimensions <= 12130 && palletsWeight >= 401 && palletsWeight <= 1000) {
-
-    //     vehicle.innerHTML = "Short Wheel Base"
-    // }
-
-
-    // //Long Wheel Base
-
-    // if (euroDimensions <= 12170 && palletsWeight >= 1001 && palletsWeight <= 1400 && palletNumberInput <= 4) {
-
-    //     vehicle.innerHTML = "Long Wheel Base"
-    // }
-
-    // //Sprinter van
-
-    // if (euroDimensions <= 12175 && palletsWeight >= 1001 && palletsWeight <= 1250) {
-
-    //     vehicle.innerHTML = "Sprinter van"
-    // }
-
-    // //Luton van
-
-    // if (euroDimensions <= 12185 && palletsWeight >= 401 && palletsWeight <= 1000) {
-
-    //     vehicle.innerHTML = "Luton van"
-    // }
-
-    // //7 & a half Tonne
-
-    // if (euroDimensions <= 12230 && palletsWeight >= 1401 && palletsWeight <= 3000) {
-
-    //     vehicle.innerHTML = "7 & a half tonne truck"
-    // }
-
-    // //18 Tonne
-
-    // if (euroDimensions <= 12250 && palletsWeight >= 3001 && palletsWeight <= 9000) {
-
-    //     vehicle.innerHTML = "18 Tonne"
-    // }
-
-    // //26 Tonne
-
-    // if (euroDimensions <= 12240 && palletsWeight >= 9001 && palletsWeight <= 15000) {
-
-    //     vehicle.innerHTML = "26 Tonne"
-    // }
-
-    // //Artic
-
-    // if (euroDimensions <= 12270 && palletsWeight >= 15001 && palletsWeight <= 24000) {
-
-    //     vehicle.innerHTML = "Artic"
-    // }
-
-    // //Megatrailer
-
-    // if (euroDimensions <= 12300 && palletsWeight >= 15001 && palletsWeight <= 24000) {
-
-    //     vehicle.innerHTML = "Megatrailer"
-    // }
 
 };
 
